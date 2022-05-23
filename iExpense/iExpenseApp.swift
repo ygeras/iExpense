5ae697a0afe13c450274aa73d197ba75d19d29e5
ygeras <86482316+Yus312@users.noreply.github.com>
2022-05-23 11:21:35 +0600

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Yuri Gerasimchuk on 23.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
